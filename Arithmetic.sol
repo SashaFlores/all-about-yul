651f56c0dcd78a61b0055b9d655a03c31721a7d8
Sasha Flores <sashaflores207@gmail.com>
2023-07-26 17:29:55 -0400

    arithmeticExample

---
+++ Arithmetic.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.19;
+
+contract Arithmetic {
+
+
+    // `bytes4(keccak256('Overflow()')`
+    bytes32 constant overflowSelector = 0x35278d1200000000000000000000000000000000000000000000000000000000;
+
+    // `bytes4(keccak256('Underflow()')`
+    bytes32 constant underflowSelector = 0xcaccb6d900000000000000000000000000000000000000000000000000000000;
+
+    // `bytes4(keccak256('ZeroDivisor()')`
+    bytes32 constant zeroDivisorSelector = 0x20e3a9a600000000000000000000000000000000000000000000000000000000;
+
+    // `bytes4(keccak256('ZeroMultiplier()')`
+    bytes32 constant zeromMultiplerSelector = 0xcdcf71c000000000000000000000000000000000000000000000000000000000;
+
+
+
+    error Overflow();
+    error Underflow();
+    error zeroDivisor();
+    error zeroMultiplier();
+
+    // Gas: 993
+    function addSolidity(uint256 a, uint256 b) public pure returns (uint256 result) {
+        return a + b;
+    }
+
+
+     // Gas: 761 
+    function addAssembly(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 result;
+
+        assembly {
+            result := add(a, b)
+        }
+
+        return result;
+    }
+
+    // Gas: 853
+    function addWithCheck(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 result;
+
+        assembly {
+            result := add(a, b)
+            if lt(result, a) {
+                mstore(0x00, overflowSelector)
+                revert(0x00, 0x20)
+            }
+        }
+
+        return result;
+    }
+
+    // Gas: 1015
+    function subSolidity(uint256 a, uint256 b) public pure returns (uint256 result) {
+        return a - b;
+    }
+
+    // Gas: 783 
+    function substractAssembly(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 result;
+
+        assembly {
+            result := sub(a, b)
+        }
+
+        return result;
+    }
+
+    // Gas: 895
+    function subtractWithCheck(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 result;
+
+        assembly {
+            result := sub(a, b)
+            if gt(result, a) {
+                mstore(0x00, underflowSelector)
+                revert(0x00, 0x20)
+            }
+        }
+
+        return result;
+    }
+
+
+    function divideSolidity(uint256 a, uint256 b) public pure returns(uint256 result) {
+        return a / b;
+    }
+
+    function divideAssembly(uint256 a, uint256 b) public pure returns(uint256) {
+        uint256 result;
+        assembly {
+            result := div(a, b)
+        }
+        return result;
+    }
+
+
+    function divideWithCheck(uint256 a, uint256 b) public pure returns(uint256) {
+        uint256 result;
+        assembly {
+            result := div(a, b)
+            if iszero(b) {
+                mstore(0x00, zeroDivisorSelector)
+                revert(0x00, 0x20)
+            }
+        }
+        return result;
+    }
+
+
+    function multiplySolidity(uint256 a, uint256 b) public pure returns(uint256 result) {
+        return a * b;
+    }
+
+    function multiplyAssembly(uint256 a, uint256 b) public pure returns(uint256) {
+        uint256 result;
+        assembly {
+            result := mul(a, b)
+        }
+        return result;
+    }
+
+    function multiplyWithCheck(uint256 a, uint256 b) public pure returns(uint256) {
+        uint256 result;
+        assembly {
+            result := mul(a, b)
+            if or(iszero(a), iszero(b)) {
+                mstore(0x00, zeromMultiplerSelector)
+                revert(0x00, 0x20)
+            }
+        }
+        return result;
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# all-about-yul
-Unlock the Power of Inline Assembly: A Smart Contract Developer's Guide
+## Unlock the Power of Inline Assembly: A Smart Contract Developer's Guide.
+
+Yul is a low level language that can be used in a standalone Yul contracts, or in Solidity contracts via an assembly block. The default dialect of Yul currently is the EVM dialect.
+
+Since the EVM is a stack-based virtual machine, it operates by set of instructions which can be categorized as:
+
+**1- Stack Instructions**
+
+- is the set of instructions that manipulate the position of values on the stack.
+- Since Yul manages local variables and control-flow, opcodes that interfere with these features are not available.
+
+*Examples of Stack Opcode:*
+
+- `pushN`   
+- `dupN`
+- `swapN`
+- `jumpN`
+
+
+**2- Arithmetic Instructions**
+
+- pops two or values from the stack, performs an arithmetic operation with, and then pushes the result.
+
+*Examples of Arithmetic Opcode:*
+
+- `add`
+- `div`
+- `mul`
+- `mod`
+
+**3- Comparison Instructions**
+
+- pops one or two values from the stack, performs a comparison and pushes the result; either False (0) or True (1).
+
+*Examples of Comparison Opcode:*
+- `lt`
+- `gt`
+- `eq`
+- `iszero`
+
+
+**4- Bitwise Instructions**
+
+- pops one or two values from the stack, performs a bitwise operations on them.
+
+*Examples of Bitwise Opcode:*
+- `and`
+- `or`
+- `xor`
+- `not`
+
+**5- Memory Instructions**
+
+- it read and writes to the memory.
+
+*Examples of Memory Opcode:*
+- `mstore`
+- `mload`
+- `mstore8`
+
+
+**6- Read Context Instructions**
+
+- it reads from the global state and execution context.
+
+*Examples of Read Context Opcode:*
+- `caller`
+- `sload`
+- `chainid`
+
+
+**7- Write Context Instructions**
+
+- it writes to the global state and the execution context.
+
+*Examples of Write Context Opcode:*
+- `call`
+- `create`
+- `sstore`
+
+### You can find a list of all `Yul` opcodes [here.](./EVM-Dialect.md)
